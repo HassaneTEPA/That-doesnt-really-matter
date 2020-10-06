150e19a320288e985c0ad54544cf751f73a050cc
Zikmiti <41624992+Zikmiti@users.noreply.github.com>
2020-10-06 18:31:45 +1100

    fix merge

--- Class/Cube.js
+++ Class/Cube.js
@@ -1,8 +1,9 @@
-function Cube(x, y, width, height) {
+function Cube(x, y, width, height, static = false) {
 
     var options = {
-        friction: 0.6,
-        restitution: 0.3
+        friction: 1,
+        restitution: 1,
+        isStatic: static,
     }
 
     this.body = Bodies.rectangle(x, y, width, height, options);
@@ -19,13 +20,15 @@ function Cube(x, y, width, height) {
 
         push();
         translate(pos.x, pos.y);
+
         rotate(angle);
+
 
         rectMode(CENTER); //permet de placer le point de physique au centre de l'objet
 
         strokeWeight(1);
-        stroke(255);
-        fill(127);
+        stroke(220);
+        fill(220);
 
 
         rect(0, 0, this.width, this.height);
--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+
+
 // Modules alias
 var Engine = Matter.Engine,
     World = Matter.World,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Body = Matter.Body,
+    Vector = Matter.Vector,
+    Mouse = Matter.Mouse;
 
 
 var engine;
 var world;
 var cubes = [];
 var sol;
+var plafond;
+var bordDroit;
+var bordGauche;
+var plateforms = [];
+var canMove = true;
+var delay = 200;
 
 //Setup Moteur Physics + création des objets 
 function setup() {
-    createCanvas(1920, 1080);
+    createCanvas(window.innerWidth, window.innerHeight);
+
+
 
 
     engine = Engine.create();
+
     world = engine.world;
     Engine.run(engine);
 
@@ -22,8 +36,27 @@ function setup() {
         isStatic: true
     }
 
-    sol = Bodies.rectangle(window.innerWidth / 2, window.innerHeight, window.innerWidth, 25, options)
+    plafond = Bodies.rectangle(0, 0, width * 2, 70, options);
+    sol = Bodies.rectangle(width / 2, height, width, 25, options);
+
+    bordDroit = Bodies.rectangle(0, 0, 25, height * 2, options);
+
+    bordGauche = Bodies.rectangle(window.innerWidth, 0, 25, height * 2, options);
+
+
     World.add(world, sol);
+    World.add(world, plafond);
+    World.add(world, bordDroit);
+    World.add(world, bordGauche);
+
+
+
+    if (plateforms.length != 0) {
+        plateforms.forEach(plateform => {
+            World.add(world, plateform);
+        });
+    }
+
 }
 
 
@@ -36,31 +69,135 @@ function draw() {
     cubes.forEach(cube => {
         cube.show();
     });
+
+    plateforms.forEach(plateform => {
+        plateform.show();
+    });
+
     fill(177);
     rectMode(CENTER);
     noStroke();
-    rect(sol.position.x, sol.position.y, width, 25)
+
+    rect(sol.position.x, sol.position.y, width, 25);
+    rect(bordGauche.position.x, height, 25, height * 2);
+    rect(0, bordDroit.position.y, 25, height * 4);
+    rect(plafond.position.x, plafond.position.y, width * 2, 25)
+
+
+    // Commandes joueur1
+    if (keyIsDown(90) && canMove) {
+        canMove = false;
+        Body.setVelocity(cubes[0].body, Vector.create(0, -10));
+        if (!canMove) {
+            setTimeout(function () {
+                canMove = true;
+            }, delay);
+        }
+    }
+
+    if (keyIsDown(68) && canMove) {
+        canMove = false;
+        Body.setVelocity(cubes[0].body, Vector.create(10, 0));
+        if (!canMove) {
+            setTimeout(function () {
+                canMove = true;
+            }, delay);
+        }
+    }
+
+    if (keyIsDown(81) && canMove) {
+        canMove = false;
+        Body.setVelocity(cubes[0].body, Vector.create(-10, 0));
+        if (!canMove) {
+            setTimeout(function () {
+                canMove = true;
+            }, delay);
+        }
+    }
+
+    // Commandes joueur2
+    if (keyIsDown(UP_ARROW) && canMove) {
+        canMove = false;
+        Body.setVelocity(cubes[1].body, Vector.create(0, -10));
+        if (!canMove) {
+            setTimeout(function () {
+                canMove = true;
+            }, delay);
+        }
+    }
+
+    if (keyIsDown(RIGHT_ARROW) && canMove) {
+        canMove = false;
+        Body.setVelocity(cubes[1].body, Vector.create(10, 0));
+        if (!canMove) {
+            setTimeout(function () {
+                canMove = true;
+            }, delay);
+        }
+    }
+
+    if (keyIsDown(LEFT_ARROW) && canMove) {
+        canMove = false;
+        Body.setVelocity(cubes[1].body, Vector.create(-10, 0));
+        if (!canMove) {
+            setTimeout(function () {
+                canMove = true;
+            }, delay);
+        }
+    }
+
+
+
 
 }
 
 
+
+
+function keyPressed() {
+    console.log(keyCode);
+    if (keyCode === 32) {
+        // for (let y = 100; y < 150; y = y + 10) {
+        //     for (let x = 100; x < 150; x = x + 10) {
+        //         var plateform = new Cube(x, y, 100, 10, true);
+        //         plateforms.push(plateform);
+        //     }
+        // }
+
+
+        let joueur1 = new Cube(2, 100, 30, 30);
+
+        cubes.push(joueur1);
+
+
+        let joueur2 = new Cube(width, 100, 30, 30);
+
+
+        cubes.push(joueur2);
+
+
+
+    }
+
+}
 
 /***************/
 /*****Event*****/
 /***************/
 
-function mouseDragged() {
-    cubes.push(new Cube(mouseX, mouseY, 20, 20));
-}
+// function mouseDragged() {
+//     cubes.push(new Cube(mouseX, mouseY, 20, 20));
+// }
 
 
-function mouseClicked() {
-    // cubes.push(new Cube(mouseX, mouseY, 80, 80));
-    i = 0;
-    while (i < 100) {
-        cubes.push(new Cube(mouseX, mouseY, random(10, 50), random(10, 50)));
-        i++
-    }
+// function mouseClicked() {
+//     cubes.push(new Cube(mouseX, mouseY, 20, 20));
 
-}
+
+// }
+
+
+
+
+
 
